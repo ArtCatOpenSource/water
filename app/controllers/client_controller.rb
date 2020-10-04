a4aff0267ef077d2e15e57f944a7f21de554b787
Barry Hoggard <barry@artsymail.com>
2020-10-03 22:24:06 -0400

    initial render without layouts or partials

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,4 +1,10 @@
 class ClientController < ApplicationController
+  prepend_view_path "vendor/templates"
+
+  before_action { headers["Content-Type"] ||= 'text/html; charset=utf-8' }
+
   def index
+    @template = Liquid::Template.parse(File.read("vendor/templates/index.liquid"))
+    render html: @template.render.html_safe
   end
 end
